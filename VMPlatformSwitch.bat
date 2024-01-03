cb384504be51d732301f181640e94fbcc1d1d14e
e-ke <89178778+e-ke@users.noreply.github.com>
2024-01-03 13:14:33 +0900

    Add VMPlatformSwitch.bat

---
+++ VMPlatformSwitch.bat
@@ -0,0 +1,27 @@
+@echo off
+SETLOCAL EnableDelayedExpansion
+chcp 65001 > nul
+REM コマンドの出力を変数に格納し、現在の状態を表示
+FOR /F "tokens=* USEBACKQ" %%F IN (`Powershell -Command "(Get-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform).State"`) DO (
+    SET state=%%F
+)
+echo VirtualMachinePlatformの状態: !state!
+
+REM ユーザー入力のプロンプト
+SET /P action="VirtualMachinePlatformの状態を変更します (0=何もしない, 1=状態を変更): "
+
+REM ユーザー入力が1の場合のみ処理を実行
+IF "!action!"=="1" (
+    REM StateがDisabledの場合に、機能を有効化
+    IF "!state!"=="Disabled" (
+        Powershell -Command "Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform"
+        echo VirtualMachinePlatformを有効化しました
+    ) ELSE IF "!state!"=="Enabled" (
+        Powershell -Command "Disable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform"
+        echo VirtualMachinePlatformを無効化しました
+    )
+) ELSE (
+    echo 何も処理は行われませんでした。
+)
+ENDLOCAL
+pause
